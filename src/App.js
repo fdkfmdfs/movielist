bd7845b984c5897b79912c2b7b72048fa8c3583d
taeook <aksehfdl65@naver.com>
2023-09-12 16:13:47 +0900

    2121212

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-
-import React from 'react';
-import './App.scss';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Users from './pages/Users';
-import Movies from './pages/Movies';
-import User from './pages/User';
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import './App.scss'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Users from './pages/Users'
+import Movies from './pages/Movies'
+import User from './pages/User'
 import About from './pages/About'
 
-
-
-
 function App() {
-  
-  return (
-    <div className='App'>
-       <Navbar />
-       <Routes>
-          <Route path="/" element={<Home />} /> 
-          <Route path="/movies" element={<Movies />} /> 
-          <Route path="/users" element={<Users />} />
-          <Route path="/users/:id"  element={<User />} />
-          <Route path="/about/:id"  element={<About />} />
-       </Routes>
-    </div>
-  );
+   return (
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+         <div className='App'>
+            <Navbar />
+            <Routes>
+               <Route path='/' element={<Home />} />
+               <Route path='/movies' element={<Movies />} />
+               <Route path='/users' element={<Users />} />
+               <Route path='/users/:id' element={<User />} />
+               <Route path='/about/:id' element={<About />} />
+            </Routes>
+         </div>
+      </BrowserRouter>
+   )
 }
 
-export default App;
+export default App
